5cf8b172af379cdbf368a2c2e37f9f17593d0487
Arunabha Mishra <102230565+MishraJi-Devloper@users.noreply.github.com>
2024-03-23 20:27:55 +0530

    Delete SWOC directory

--- SWOC/swoc_app.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Comment(models.Model):
-    author = models.CharField(max_length=100)
-    comment_text = models.TextField()
-
-    def __str__(self):
-        return self.author
-
--- SWOC/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('http://127.0.0.1:8000/', views.homepage, name='homepage'),
-    # Add more URL patterns for other views as needed
-]
